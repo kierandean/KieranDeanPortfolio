6a33a7ddc7df6d25df875cc27e7248af605e6198
Kieran Thompson <3thomk39@solent.ac.uk>
2019-04-17 16:55:30 +0100

    Ariticle page uniformed + responsive. **todo: write article, fix window close milkmodal. ++ updated all navbars

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-var modal = document.getElementById("mymodal");
-var btn =document.getElementById("mybtn");
-var span = document.getElementsByClassName("close");
-
-btn.onclick = function(){
+var modal = document.getElementById('milkmodal');
+var btn = document.getElementById("milkBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
   modal.style.display = "block";
-}
-span.onclick = function(){
+  btn.style.color = "rgb(128, 0, 128)"
+} 
+span.onclick = function() {
   modal.style.display = "none";
 }
-window.onclick = function(event){
-  if(event.target == modal){
-    modal.style.display="none";
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
