78694d247b753f78b7fd3caffd15454825b7bc0e
Kieran Thompson <3thomk39@solent.ac.uk>
2019-04-23 12:42:54 +0100

    article page: -Broke java +update design
    Lending:
    +updated design
    
    Contact:
    +updated design
    -finish form

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
 var mmodal = document.getElementById('milkmodal');
 var mbtn = document.getElementById("milkBtn");
 var mspan = document.getElementsByClassName("close")[0];
+
 mbtn.onclick = function () {
-    mmodal.style.display = "block";
-    mbtn.style.color = "rgb(128, 0, 128)"
-  }
-  mspan.onclick = function () {
+  mmodal.style.display = "block";
+  mbtn.style.color = "rgb(128, 0, 128)"
+}
+mspan.onclick = function () {
+  mmodal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == mmodal) {
     mmodal.style.display = "none";
   }
-  window.onclick = function (event) {
-    if (event.target == mmodal) {
-      mmodal.style.display = "none";
-    }
-  }
+}
+
+function click() {
+
+}
+
 function main() {
-
-
-  
+  mmodal.onclick = click();
   console.log("in main function")
-  }
+}
